53757c527c0fa88511ff970816dd022ee3ec9b11
maicol <maykolandres4@gmail.com>
2023-02-02 04:56:33 -0500

    piping, does the same thing but faster

--- stream.js
+++ stream.js
@@ -4,12 +4,16 @@ const readStream = fs.createReadStream('./docs/blog2.txt', {encoding: 'utf-8'});
 
 //write chunk
 
-const writeStream = fs.createWriteStream('./docs/blog4.txt')
+const writeStream = fs.createWriteStream('./docs/blog3.txt')
 
-readStream.on('data', (chunk) => {
+/* readStream.on('data', (chunk) => {
     console.log('---NEW CHUNK---');
     console.log(chunk)
 
     writeStream.write('\nNEW CHUNK\N');
     writeStream.write(chunk);
-});
+}); */
+
+//piping
+
+readStream.pipe(writeStream);
