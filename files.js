ff117b86c6a0b9ba1e1524e5296dbaf9bb8aa41c
maicol <maykolandres4@gmail.com>
2023-02-02 04:34:40 -0500

    create and delete folders

--- files.js
+++ files.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 //read 
-
+/* 
 fs.readFile('./docs/blog.txt', (err, data) => {
    if(err){
     console.log(err);
@@ -15,8 +15,26 @@ fs.readFile('./docs/blog.txt', (err, data) => {
 fs.writeFile('./docs/blog.txt', 'hello again', () => {
     console.log('done writing')
 })
-
+ */
 
 //directories
+
+if(!fs.existsSync('./assets')){
+    fs.mkdir('./assets', (err) => {
+        if(err) {
+          console.log(err);
+        }
+      
+        console.log("folder created")
+    })
+} else {
+    fs.rmdir('./assets', (err) => {
+        if(err) {
+            console.log(err)
+        }
+        console.log('folder deleted')
+    })
+}
+
 
 //delete
