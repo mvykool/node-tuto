f6aece426734d100d2a3c6be314a649a0b2b1b03
maicol <maykolandres4@gmail.com>
2023-02-03 05:12:21 -0500

    testing url and method

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
 });
 
 server.listen(3000, 'localhost', () => {
