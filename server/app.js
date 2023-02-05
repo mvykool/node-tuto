dfe50567996d89387b9fe875955171d3d5810514
maicol <maykolandres4@gmail.com>
2023-02-05 11:14:13 -0500

    added more files

--- server/app.js
+++ server/app.js
@@ -1,34 +1,38 @@
 const express = require('express');
 
-//express app
-
+// express app
 const app = express();
 
-//register view engine
-
-app.set('view engine', 'ejs');
-
-
-//listen for request
-
+// listen for requests
 app.listen(3000);
 
+
+//middleware of static
+
+app.use(express.static('public'));
+
+// register view engine
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews');
+
 app.get('/', (req, res) => {
-   res.sendFile('./views/index.html', { root: __dirname });
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname });
- });
+  res.render('about', { title: 'About' });
+});
 
- //redirects
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+});
 
- app.get('/about-me', (req, res) => {
-    res.redirect('/about');
- })
-
- //404 page MUST BE AT THE BOTTOM
-
- app.use((req,res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname});
- })
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
