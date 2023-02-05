973b09a4bc45d1ff6194b6a95b8328a82aa7d894
maicol <maykolandres4@gmail.com>
2023-02-05 08:02:49 -0500

    added redirect

--- server/server.js
+++ server/server.js
@@ -23,6 +23,11 @@ const server = http.createServer((req, res) => {
             path += 'about.html'
             res.statusCode = 200;
             break
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break    
         default:
             path += '404.html'
             res.statusCode = 404;
