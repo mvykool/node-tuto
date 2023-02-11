30da9e007c3e62eb265c7953f176eafb8087f531
maicol <maykolandres4@gmail.com>
2023-02-10 23:09:38 -0500

    created folder and added more stuff in there

--- server/app.js
+++ server/app.js
@@ -74,35 +74,6 @@ app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
 });
 
-//blog routes
-
-app.get('/blogs', (req, res) => {
-  Blog.find().sort({createdAt: -1})
-    .then((result) => {
-      res.render('index', {title: 'ALl Blogs', blogs: result});
-    })
-     .catch((err) => {
-      console.log(err)
-     })
-})
-
-//post request
-app.post('/blogs', (req, res) => {
-   const blog = new Blog(req.body);
-
-   blog.save()
-     .then((result) => {
-      res.redirect('/blogs');
-     })
-      .catch((err) => {
-        console.log(err);
-      });
-});
-
-
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
-});
 
 // 404 page
 app.use((req, res) => {
